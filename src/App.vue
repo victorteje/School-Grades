7fba673abf79681d5b4f5969f86757fd3128f04d
victorteje <vteje2@gmail.com>
2023-07-16 10:46:27 +0200

    basic styles added

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,67 @@
 <script setup>
   import Students from './components/Students.vue'
+  import { ref } from 'vue';
+  const studentData = ref('');
+  const addData = (data) => {
+
+    const newRow = document.createElement("tr");
+    
+
+  }
 </script>
 
 <template>
-  <header>
-    <h1>School Grades</h1>    
-  </header>
-  <main>
-    <Students/>
-    <h2 id="studentsName">{{ selectStudent }}</h2>
-    <tr id="gradesTable">
-      <th class="tableHeader">Subject</th>
-      <th class="tableHeader">Grade</th>
-    </tr>
-  </main>
+  <body>
+    <header>
+      <h1>School Grades</h1>    
+    </header>
+    <main>
+      <Students  @send-data="addData"/>
+      <h2 id="studentsName">{{ studentData[0] }}</h2>
+      <table id="gradesTable">
+        <tr>
+          <th class="tableHeader">Subject</th>
+          <th class="tableHeader">Grade</th>
+        </tr>
+        <tr>
+          <td>Prueba</td>
+          <td>Prueba</td>
+        </tr>
+      </table>
+    </main>
+  </body>
 </template>
 
 <style scoped>
+
+
+  body {
+    background: linear-gradient(to bottom right, #edf2fb, #ccdbfd);
+    width: 100vw;
+    height: 100vh;
+  }
+
+  main {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
+
+  h1 {
+    text-align: center;
+  }
+
+  th, td {
+    text-align: center;
+    border: 1px solid;
+    padding: 8px 16px;
+    width: 14vw;
+  }
+
+  th {
+    background-color: #abc4ff;
+  }
+
 
 </style>
--- src/components/Students.vue
+++ src/components/Students.vue
@@ -1,48 +1,59 @@
 <script setup>
     import { ref } from 'vue';
-    const selectStudent = ref('');
-    const selectSubject = ref('');
-    const selectGrade = ref('');
+    const student = ref('');
+    const subject = ref('');
+    const grade = ref('');
+    const studentData = [student.value, subject.value, grade.value]
+    // const emit = defineEmits(['student', 'subject', 'grade']);
 
-    const submit = () => {
-        
-    }
+    // const studentData = ref([student.value, subject.value, grade.value]);
+
 </script>
 
 <template>
-    <select v-model="selectStudent">
-        <option disabled value="">Select a student</option>
-        <option>Diego Córdoba</option>
-        <option>Manuela Grajales</option>
-        <option>María Villaverde</option>
-    </select>
-    <select v-model="selectSubject">
-        <option disabled value="">Select a subject</option>
-        <option>HTML + CSS</option>
-        <option>JavaScript</option>
-        <option>Vue</option>
-        <option>React</option>
-        <option>Java</option>
-        <option>PHP</option>
-    </select>
-    <select v-model="selectGrade">
-        <option disabled value="">Select the student's grade</option>
-        <option>0</option>
-        <option>1</option>
-        <option>2</option>
-        <option>3</option>
-        <option>4</option>
-        <option>5</option>
-        <option>6</option>
-        <option>7</option>
-        <option>8</option>
-        <option>9</option>
-        <option>10</option>
-    </select>
-    <button id="submit" @click="submit()">Submit</button>
-    <!-- <h2 id="studentsName">{{ selectStudent }}</h2> -->
+    <form>
+        <select id="student" v-model="student">
+            <option disabled value="">Select a student</option>
+            <option>Diego Córdoba</option>
+            <option>Manuela Grajales</option>
+            <option>María Villaverde</option>
+        </select>
+        <select id="subject" v-model="subject">
+            <option disabled value="">Select a subject</option>
+            <option>HTML + CSS</option>
+            <option>JavaScript</option>
+            <option>Vue</option>
+            <option>React</option>
+            <option>Java</option>
+            <option>PHP</option>
+        </select>
+        <select id="grade" v-model="grade">
+            <option disabled value="">Select the student's grade</option>
+            <option>0</option>
+            <option>1</option>
+            <option>2</option>
+            <option>3</option>
+            <option>4</option>
+            <option>5</option>
+            <option>6</option>
+            <option>7</option>
+            <option>8</option>
+            <option>9</option>
+            <option>10</option>
+        </select>
+        <button id="submit" @click="$emit('sendData', studentData)">Submit</button>
+        <!-- <h2 id="studentsName">{{ selectStudent }}</h2> -->
+    </form>
 </template>
 
 <style scoped>
+    form {
+        display: flex;
+        gap: 24px;
+    }
 
+    button {
+        border-radius: 4px;
+        padding: 2px 8px;
+    }
 </style>
